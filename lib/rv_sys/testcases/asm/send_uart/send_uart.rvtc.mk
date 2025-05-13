b6c9dfb4f266ae113c90f4a40d66d2193e1995e1
Florian Huemer <fhuemer@ecs.tuwien.ac.at>
2025-05-13 16:23:56 +0200

    update send_uart testcase

--- lib/rv_sys/testcases/asm/send_uart/send_uart.rvtc.mk
+++ lib/rv_sys/testcases/asm/send_uart/send_uart.rvtc.mk
@@ -1,9 +1,9 @@
-cur_dir:=$(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+send_uart_dir:=$(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 
 RV_TESTCASES+=SEND_UART
 
 SEND_UART_NAME:=send_uart
 SEND_UART_TB:=send_uart_tb
-SEND_UART_ELF:=$(cur_dir)/send_uart.elf
+SEND_UART_ELF:=$(send_uart_dir)/send_uart.elf
 
-SEND_UART_VHDL_FILES:=$(cur_dir)/send_uart_tb.vhd
+SEND_UART_VHDL_FILES:=$(send_uart_dir)/send_uart_tb.vhd
